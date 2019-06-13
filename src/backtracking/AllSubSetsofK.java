6944df6893c9d033764470f66861498dd087443a
Raja Chakraborty <rajachakraborti@gmail.com>
2019-06-13 09:07:33 -0400

    removed copied n

--- src/backtracking/AllSubSetsofK.java
+++ src/backtracking/AllSubSetsofK.java
@@ -7,25 +7,25 @@ import java.util.stream.IntStream;
 
 public class AllSubSetsofK {
     public static void main(String[] args) {
-        List<List<Integer>> l  = new AllSubSetsofK().compute(5, 3);
+        List<List<Integer>> l  = new AllSubSetsofK().compute(5);
         l.stream().forEach(System.out::println);
 
     }
 
-    private List<List<Integer>> compute(int n, int k) {
+    private List<List<Integer>> compute(int n) {
         List<List<Integer>> results = new ArrayList<>();
         List<Integer> range = IntStream.range(1, n+1).boxed().collect(Collectors.toList());
-        compute(n, k , new ArrayList<Integer>(), range, results);
+        compute(n,  new ArrayList<Integer>(), range, results);
         return results;
     }
 
-    private void compute(int n, int k, List<Integer> prefixList,
+    private void compute(int n,  List<Integer> prefixList,
                          List<Integer> originalList, List<List<Integer>> results) {
         results.add(new ArrayList(prefixList));
 
         for(int i = 0; i < originalList.size(); i++) {
             prefixList.add(originalList.get(i));
-            compute(n, k , prefixList, originalList.subList(i+1, originalList.size()), results);
+            compute(n , prefixList, originalList.subList(i+1, originalList.size()), results);
             prefixList.remove(prefixList.size() - 1);
         }
     }
